fe5c572a56831ec0a2d5ebd981862e558a2d2d8d
Kuchi <matejkucharcik08@gmail.com>
2024-05-08 20:05:30 +0200

    .

--- proboj-prask/klienti/klopac/player.py
+++ proboj-prask/klienti/klopac/player.py
@@ -22,39 +22,6 @@ class MyPlayer(ProbojPlayer):
     self.turret_lvl: int
     self.world: list[Optional[Unit]] = []
     """
-    moje_jednotky: list[tuple[int, Unit]]
-    jeho_jednotky: list[tuple[int, Unit]]
-    buy_queue = []
-    attack_stage = 0
-    
-    
-    def zivoty_jednotiek(self):
-        self.log(self.moje_jednotky, self.jeho_jednotky)
-
-        def sucet(jednotky):
-            return sum(jednotka.hp for _, jednotka in jednotky)
-
-        return sucet(self.moje_jednotky), sucet(self.jeho_jednotky)
-
-    def kolko_kol_protivnik_pride_k_nasemu_domceku(self):
-        hp_moje, hp_jeho = self.zivoty_jednotiek()
-        if hp_jeho == 0:
-            return INF
-        vzdialenost = min(i - u.attack_range for i, u in self.jeho_jednotky)
-        maximalny_utok = max(unit_attack_dmg)
-        cas = hp_moje / maximalny_utok + vzdialenost
-        
-        return cas
-
-    def kolko_mozem_minut_bez_ohrozenia_domceka(self):
-        cas = self.kolko_kol_protivnik_pride_k_nasemu_domceku()
-        if cas == INF:
-            return INF
-        if cas <= MIN_SAFE_DIST:
-            return -1
-        peniaze = self.money + income[self.income_lvl] * cas
-        mozem_minut = max(0, peniaze - unit_cost[UnitType.BAGER.value])
-        return mozem_minut
 
             
         
@@ -68,18 +35,20 @@ class MyPlayer(ProbojPlayer):
         Túto funkciu máte naprogramovať
         """        
         stratoska = 2
-        
+        max = 5
         pocet_nepriatelov = 0
-        list = self.world
-        for i in range(0, len(list)-1):
-            jednotka = list[i]
-            if not jednotka.owner == self.name:
-                pocet_nepriatelov += 1
-        
-        if pocet_nepriatelov > 1:
-            stratoska = 2
-        else:
-            stratoska = 1
+        zoznwm = self.world
+        for i in range(0, len(zoznwm)-1):
+            jednotka = zoznwm[i]
+            if jednotka is not None:
+                if not jednotka.owner == self.name:
+                    pocet_nepriatelov += 1
+                    if i < 10:
+                        stratoska = 2
+                    elif pocet_nepriatelov > 0:
+                        stratoska = 4
+                    else:
+                        stratoska = 1
 
         if stratoska == 1:
             return Command.INCOME    
@@ -89,7 +58,8 @@ class MyPlayer(ProbojPlayer):
             return Command.BAGER  
         elif stratoska == 4:
             ##self.Rudko_je_najsamsuper()
-            Command.VALEC
+            return Command.VALEC
+        pass
            
         
         
